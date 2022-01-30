c869aed907725d4b83e8391809b9c3a7aac96269
Matt Rothenberg <mattrothenberg@gmail.com>
2022-01-30 12:55:12 -0500

    basic example

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,11 @@ function App() {
   return (
     <div className="grid grid-cols-2 p-4">
       <div className="aspect-square relative">
-        <Stage render={(size) => <Basic radius={72} size={size} />}></Stage>
+        <Stage
+          render={(size) => {
+            return <Basic radius={size.width / 3} size={size} />;
+          }}
+        ></Stage>
       </div>
     </div>
   );
--- src/variations/basic.tsx
+++ src/variations/basic.tsx
@@ -1,15 +1,44 @@
+import { useAnimationFrame } from "framer-motion";
+import { useState } from "react";
+import { degreesToRadians } from "../lib";
+
 export function Basic(props: VariationProps) {
   const { radius, size } = props;
   const { width } = size;
+  const [angle, setAngle] = useState(270);
+
+  useAnimationFrame((t) => {
+    setAngle((currAngle) => currAngle - 1);
+  });
+
+  const cx = width / 2 + (radius / 2) * Math.cos(degreesToRadians(angle));
+  const cy = width / 2 + (radius / 2) * Math.sin(degreesToRadians(angle));
+
   return (
-    <g>
-      <ellipse
-        className="stroke-gray-800 fill-transparent stroke-2"
-        cx={width / 2}
-        cy={width / 2}
-        rx={radius}
-        ry={radius}
-      />
-    </g>
+    <>
+      <g>
+        <ellipse
+          className="stroke-gray-800 fill-transparent stroke-2"
+          cx={width / 2}
+          cy={width / 2}
+          rx={radius}
+          ry={radius}
+        />
+        <ellipse
+          className="stroke-gray-800 fill-transparent stroke-2"
+          cx={cx}
+          cy={cy}
+          rx={radius / 2}
+          ry={radius / 2}
+        />
+        <line
+          x1={width / 2}
+          y1={width / 2 + radius * Math.sin(degreesToRadians(angle))}
+          x2={cx}
+          y2={cy}
+          className="stroke-pink-600 stroke-2"
+        />
+      </g>
+    </>
   );
 }
